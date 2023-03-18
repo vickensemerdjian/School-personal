dbf86592a1d8cd20501a671f78048e3be66c278c
vicken <57333393+vickensemerdjian@users.noreply.github.com>
2023-03-17 17:47:31 -0700

    Update GEORGE(readable).cpp

--- GEORGE/GEORGE(readable).cpp
+++ GEORGE/GEORGE(readable).cpp
@@ -28,7 +28,7 @@ brain Brain;
 
 // Robot configuration code.
 servo BillGate = servo(Brain.ThreeWirePort.A);
-motor OnlyMotor = motor(PORT10, ratio18_1, true);
+motor OnlyMotor = motor(PORT9, ratio18_1, true);
 
 optical AllSeeing = optical(PORT21);
 servo FloodGate = servo(Brain.ThreeWirePort.B);
@@ -59,27 +59,26 @@ int SortState = 0;
 
     if (LimitSwitch.pressing()) {         //Metal Ball
       BillGate.setPosition(-48,degrees);
-     SortState=1;
-     AllSeeing.setLight(ledState::off);
      FloodGate.setPosition(10,degrees);
-     wait(0.5,seconds);
+       SortState=1;
+     wait(1,seconds);
   }
   
-   else if((AllSeeing.hue()>39)&&(AllSeeing.hue()<43)){ //white ball (44)
+   else if((AllSeeing.hue()>41)&&(AllSeeing.hue()<58)){ //white ball (44)
      Brain.Screen.print(AllSeeing.hue());
      BillGate.setPosition(-50,degrees);
-     SortState=1;
+   SortState=1;
      FloodGate.setPosition(50,degrees);
    }
-    else if((AllSeeing.hue()>28)&&(AllSeeing.hue()<34)){ //wood ball(32)
+    else if((AllSeeing.hue()>28)&&(AllSeeing.hue()<40)){ //wood ball(32)
       Brain.Screen.print(AllSeeing.hue());
      BillGate.setPosition(-50,degrees);
-     SortState=1;
+       SortState=1;
       FloodGate.setPosition(19,degrees);
    }
  }
    
-//void PressFunction(){
+
 
 
 
@@ -87,33 +86,22 @@ int SortState = 0;
 
  
 
-  void Turnstile(){
-  //  OnlyMotor.spinFor(reverse,0.3,turns);
-    OnlyMotor.setVelocity(2,percent);
-    OnlyMotor.spin(reverse);
 
-   // wait(.5,seconds);
-  //  OnlyMotor.stop();
-  }
+     
 int main() {
 BillGate.setPosition(50,degrees);
+ OnlyMotor.setVelocity(1,percent);
+    OnlyMotor.spin(reverse);
   while(1==1){
     EyeBall();
-    SortState=0;
-    while(SortState==0){
-      Brain.Timer.reset();
-      while(Brain.Timer.value()<2){ //this incriments the turnstile slowly so it doesnt accept many marbles at once
+      while(SortState==0){
       SortingStuff(); 
-           
-      }
-      Turnstile();  
-        
-    }
- 
+     }
+     SortState=0;
     wait(.5,seconds);
     Brain.Screen.clearLine(1);
     BillGate.setPosition(50,degrees);
-}
+  }
 }
 
 
