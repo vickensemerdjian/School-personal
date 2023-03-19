b89d435ee7c336ec9bdef8c2e8aad19a276a7bc6
thendermanslime <unstablecanaries@gmail.com>
2023-03-19 15:29:34 -0700

    COMMENTED - final(working)

--- GEORGE/GEORGE(readable).cpp
+++ GEORGE/GEORGE(readable).cpp
@@ -38,7 +38,7 @@ limit LimitSwitch = limit(Brain.ThreeWirePort.C);
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       {Chris Morales, Vicken Semerdjian}                        */
+/*    Authors:       {Chris Morales, Vicken Semerdjian}                       */
 /*    Created:      {March 2023}                                              */
 /*    Description:  V5 project                                                */
 /*                                                                            */
@@ -50,31 +50,31 @@ limit LimitSwitch = limit(Brain.ThreeWirePort.C);
 // Allows for easier use of the VEX Library
 using namespace vex;
 
- void EyeBall(){
+ void EyeBall(){ //sets light on the sensor to on and sets the power to 50%
    AllSeeing.setLight(ledState::on);
    AllSeeing.setLightPower(50,percent);
  }
 int SortState = 0;
  void SortingStuff(){
 
-    if (LimitSwitch.pressing()) {         //Metal Ball
-      BillGate.setPosition(-48,degrees);
-     FloodGate.setPosition(10,degrees);
+    if (LimitSwitch.pressing()) {         //Metal Ball, if the ball is metal, it depresses the limit switch.
+      BillGate.setPosition(-48,degrees); //opens the gate
+     FloodGate.setPosition(10,degrees); //changes the sorting rail to the metal ball position
        SortState=1;
      wait(1,seconds);
   }
   
-   else if((AllSeeing.hue()>41)&&(AllSeeing.hue()<58)){ //white ball (44)
-     Brain.Screen.print(AllSeeing.hue());
-     BillGate.setPosition(-50,degrees);
+   else if((AllSeeing.hue()>41)&&(AllSeeing.hue()<58)){ //white ball (44), if the ball is white, it is between 41 and 58
+     Brain.Screen.print(AllSeeing.hue()); //prints the hue to the screen
+     BillGate.setPosition(-50,degrees); //opens the gate
    SortState=1;
-     FloodGate.setPosition(50,degrees);
+     FloodGate.setPosition(50,degrees); //changes the sorting rail to the white ball position
    }
     else if((AllSeeing.hue()>28)&&(AllSeeing.hue()<40)){ //wood ball(32)
-      Brain.Screen.print(AllSeeing.hue());
-     BillGate.setPosition(-50,degrees);
+      Brain.Screen.print(AllSeeing.hue()); //prints the hue to the screen
+     BillGate.setPosition(-50,degrees); //opens the gate
        SortState=1;
-      FloodGate.setPosition(19,degrees);
+      FloodGate.setPosition(19,degrees); //changes the sorting rail to the wood ball position
    }
  }
    
@@ -89,18 +89,18 @@ int SortState = 0;
 
      
 int main() {
-BillGate.setPosition(50,degrees);
- OnlyMotor.setVelocity(1,percent);
-    OnlyMotor.spin(reverse);
+BillGate.setPosition(50,degrees); //sets the gate to closed
+ OnlyMotor.setVelocity(1,percent); //sets the motor to 1% power
+    OnlyMotor.spin(reverse); //makes the motor constantly spin at a low speed to only allow one ball through at once.
   while(1==1){
-    EyeBall();
-      while(SortState==0){
-      SortingStuff(); 
+    EyeBall(); //turns on the sensor and light
+      while(SortState==0){ //while the ball is not sorted
+      SortingStuff(); //keeps checking to see if any of the sort conditions are met
      }
-     SortState=0;
-    wait(.5,seconds);
-    Brain.Screen.clearLine(1);
-    BillGate.setPosition(50,degrees);
+     SortState=0; //resets the sort state so that it can sort again
+    wait(.5,seconds); //waits so the ball has time to be sorted so the sensor doesn't get confused or skip
+    Brain.Screen.clearLine(1); //clears the screen so the hue doesn't get printed over and over
+    BillGate.setPosition(50,degrees); //closes the gate
   }
 }
 
